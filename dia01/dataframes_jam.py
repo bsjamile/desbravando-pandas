46a485f2f28bc7156d824d48d3186cfbcdf9cef6
Jamile <jamilebarroso0@gmail.com>
2024-08-19 21:48:27 -0300

    desbravando series e dataframes

---
+++ dia01/dataframes_jam.py
@@ -0,0 +1,76 @@
+# %%
+import pandas as pd
+# %%
+data = {
+    "nome":["teo","nah","lara","maria"],
+    "sobrenome":["calvo","ataide","calvo","calvo"],
+    "idade":[31,32,31,2]
+}
+
+data
+# %%
+data["idade"]
+# %%
+#Dataframe é um conjunto de series
+df = pd.DataFrame(data)
+df
+# %%
+df['idade'][0] #index 0
+# %%
+df['idade'].iloc[0]#posicao 0
+# %%
+type(df['sobrenome'])
+# %%
+df['sobrenome'].iloc[2]#posicao 2 
+# %%
+df['sobrenome'][2] #index 2
+# %%
+type(df.iloc[0]) #se acessar um indice (nesse caso, a primeira linha) do dataframe vai retornar uma serie
+# %%
+df.iloc[0]#posicao
+#linha = o nome da minha serie é 0(indice diferente do 0 da posicao) e os indices é o nome da coluna correspondente(nome, sobrenome, idade)
+# %%
+df['nome'] #o nome dessa serie é nome
+# %%
+# Dataframe pandas nada mais é do que um conjunto de series, seja obter as series por meio das linhas ou por meio das colunas
+# %%
+df
+# %%
+df.index=[3,2,1,0]
+df
+# %%
+df.loc[0] #retorna o index, o nome da serie vai ser o index=0 
+# %%
+df.iloc[0]#retorna a posicao o nome da serie vai ser o index (3)
+# %%
+df.columns #nome das colunas = atributos = tudo que minha serie é/tem
+# metodo= ações, tudo que é capaz de fazer
+## %%
+#serie_idade. o que aparece em cubo é um metodo = são verbos, ações
+#o restante é atributo = é uma carateristica = sao coisas que minha serie é, carrega
+# %%
+df.info() #object = tudo que não é numérico
+# %%
+df.info(memory_usage="deep") #qnt de memoria consumida
+# %%
+df.dtypes #indice é o nome da coluna e objeto é tudo que não é numerico
+# %%
+df.describe() #aplica nas colunas numericas as estatisticas descritivas
+# %%
+df['peso']=[80,53,65,14]
+sumario = df.describe()
+sumario
+# %%
+sumario['peso']['mean']
+# %%
+sumario['idade']['mean']
+
+# %%
+df.head()#cabeça
+# %%
+df.head(2)
+# %%
+df.tail()#rabo/ponta
+# %%
+df.tail(2)
+# %%
--- dia01/series_jam.py
+++ dia01/series_jam.py
@@ -22,7 +22,7 @@ variancia
 # %%
 idades
 # %%
-series_idades = pd.Series(idades)
+series_idades = pd.Series(idades)#clase chamada series do pandas 
 series_idades
 # %%
 series_idades.mean()
@@ -42,4 +42,60 @@ moda_series
 series_idades.quantile(0.25) #primeiro quartil
 # %%
 series_idades.describe()
+# %%
+series_idades.shape #caracteristica, atributo -> shape = tupla que me diz quantas linhas minhas serie tem
+# %%
+#serie_idade. o que aparece em cubo é um metodo = são verbos, ações
+#o restante é atributo = é uma carateristica = sao coisas que minha serie é, carrega
+#navegando na lista
+idades[1]
+# %%
+#navenga na serie
+series_idades[1]#retorna o index pq ele é 1
+# %%
+series_idades
+# %%
+series_idades.index
+# %%
+#alterando index da serie
+series_idades.index = ['t','e','o','c']
+# %%
+series_idades
+# %%
+#navegando nos indices novos
+series_idades['c']#retorna o index
+# %%
+series_idades[0]#retorna a posicao
+# %%
+series_idades.iloc[0]#iloc = retorna a POSICAO
+
+# %%
+series_idades.loc['c']#loc = retorna o INDICE
+# %%
+series_idades.index=[40,10,30,20]
+# %%
+series_idades
+# %%
+series_idades.loc[30]#loc = retorna o INDICE
+# %%
+series_idades.iloc[2]#iloc = retorna a POSICAO
+# %%
+series_idades.iloc[-1]#iloc = -1 retorna a ULTIMA POSICAO
+# %%
+series_idades.iloc[2:4]
+# %%
+series_idades.iloc[0:2] #se a localizacao pra vc for relevante, utilizar o ILOC
+# %%
+series_idades.loc[30]
+# %%
+series_idades[30] #mesma coisa do LOC
+# %%
+series_idades.name = 'idades'
+#a serie vem sem nome mas posso colocar um nome nela tipo uma coluna do excel
+# %%
+series_idades #aprece com nome
+# %%
+#criando a serie e definindo um nome
+series_idades = pd.Series(idades,name='idades')
+series_idades
 # %%
